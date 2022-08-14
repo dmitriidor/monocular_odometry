6a8e253c552deed988d30bb65c565b1f855a02ce
NaughtiusNaximus <dimador4@gmail.com>
2022-08-14 20:10:58 +0300

    Additional filtering for CUDA

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ using namespace cv;
 int main(int, char**) {
 
 
-    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_2.avi");
+    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_3.avi");
 
     if(!cap.isOpened()){
         cout << "Error opening video stream or file" << endl;
@@ -33,7 +33,7 @@ int main(int, char**) {
 
 
     Mat frame_1, frame_2; 
-    cap.set(CAP_PROP_POS_MSEC, 1000);
+    cap.set(CAP_PROP_POS_MSEC, 0);
     cap >> frame_1;
 
     cuda::GpuMat gpu_frame_1;
@@ -56,14 +56,14 @@ int main(int, char**) {
     gpu_frame_1 = gpu_frame_2; 
     surf(gpu_frame_2, cuda::GpuMat(), gpu_keys_2, gpu_descriptors_2);
 
-    Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_L2);
+    Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_L1);
 
     for(int iter = 1; iter < 100+1; iter++) {
 
         gpu_keys_1 = gpu_keys_2; 
         gpu_descriptors_1 = gpu_descriptors_2;
         gpu_frame_1 = gpu_frame_2; 
-        cap.set(CAP_PROP_POS_MSEC, 1000*iter);
+        cap.set(CAP_PROP_POS_MSEC, 10*iter);
         cap >> frame_2;
 
         gpu_frame_2.upload(frame_2); 
@@ -87,54 +87,60 @@ int main(int, char**) {
         // vector< vector<DMatch> > matches; 
         // matcher->knnMatch(gpu_descriptors_1, gpu_descriptors_2, matches, 2);
 
-        vector<DMatch> matches; 
-        matcher->match(gpu_descriptors_1, gpu_descriptors_2, matches);
+        vector< vector<DMatch> > matches; 
+        matcher->knnMatch(gpu_descriptors_1, gpu_descriptors_2, matches, 2);
 
-        // vector<DMatch> filtered_matches; 
-        // float ratio = 0.75; 
-        // for(int i = 0; i < matches.size(); i++) {
-        //     if(matches[i][0].distance < matches[i][1].distance*ratio) {
-        //         filtered_matches.push_back(matches[i][0]);
-        //     }
-        // }
+        vector<DMatch> filtered_matches; 
+        float ratio = 0.75; 
+        for(int i = 0; i < matches.size(); i++) {
+            if(matches[i][0].distance < matches[i][1].distance*ratio) {
+                filtered_matches.push_back(matches[i][0]);
+            }
+        }
 
-        // vector<Point2f> filtered_points_1(filtered_matches.size(), Point2f(0, 0));
-        // vector<Point2f> filtered_points_2(filtered_matches.size(), Point2f(0, 0)); 
-        // for(int i = 0; i < filtered_matches.size(); i++) {
-        //     filtered_points_1[i] = keys_1[filtered_matches[i].queryIdx].pt;
-        //     filtered_points_2[i] = keys_2[filtered_matches[i].trainIdx].pt;
-        // }
+        surf.downloadKeypoints(gpu_keys_1, keys_1);
+        surf.downloadKeypoints(gpu_keys_2, keys_2);
 
-        // Mat i_mat = (Mat_<double>(3,3) << 73, 0, 400, 0, 73, 400, 0, 0, 1);
-        // Mat e_mat; 
-        // e_mat = findEssentialMat(filtered_points_1, filtered_points_2, i_mat, RANSAC);
+        vector<Point2f> filtered_points_1(filtered_matches.size(), Point2f(0, 0));
+        vector<Point2f> filtered_points_2(filtered_matches.size(), Point2f(0, 0)); 
+        for(int i = 0; i < filtered_matches.size(); i++) {
+            filtered_points_1[i] = keys_1[filtered_matches[i].queryIdx].pt;
+            filtered_points_2[i] = keys_2[filtered_matches[i].trainIdx].pt;
+        }
 
-        // cout << e_mat << "\n" <<endl;
+        Mat i_mat = (Mat_<double>(3,3) << 73, 0, 400, 0, 73, 400, 0, 0, 1);
+        Mat e_mat; 
 
-        // Mat rot_mat, t_vec; 
-        // recoverPose(e_mat, filtered_points_1, filtered_points_2, rot_mat, t_vec);
+        try {                    
+            e_mat = findEssentialMat(filtered_points_1, filtered_points_2, i_mat, RANSAC);
 
-        // cout << rot_mat << "\n" << endl;
-        // cout << t_vec << endl; 
+            Mat rot_mat, t_vec; 
+            recoverPose(e_mat, filtered_points_1, filtered_points_2, rot_mat, t_vec);
 
-        // cout << endl;
+            cout << rot_mat << "\n" << endl;
+            cout << t_vec << endl; 
 
-        // vector< vector<float> > rotations  {{rot_mat.at<double>(0, 0), rot_mat.at<double>(0, 1), rot_mat.at<double>(0, 2),},
-        //                                     {rot_mat.at<double>(1, 0), rot_mat.at<double>(1, 1), rot_mat.at<double>(1, 2),},
-        //                                     {rot_mat.at<double>(2, 0), rot_mat.at<double>(2, 1), rot_mat.at<double>(2, 2),}};
+            cout << endl;
+
+            vector< vector<float> > rotations  {{rot_mat.at<double>(0, 0), rot_mat.at<double>(0, 1), rot_mat.at<double>(0, 2),},
+                                                {rot_mat.at<double>(1, 0), rot_mat.at<double>(1, 1), rot_mat.at<double>(1, 2),},
+                                                {rot_mat.at<double>(2, 0), rot_mat.at<double>(2, 1), rot_mat.at<double>(2, 2),}};
+                
+            AngleConverter converter; 
+            converter.SetAndConvertR(rotations);
             
-        // AngleConverter converter; 
-        // converter.SetAndConvertR(rotations);
-        
-        // vector<float> angles(3, 0);
-        // angles = converter.GetYawPitchRollSTD();
+            vector<float> angles(3, 0);
+            angles = converter.GetYawPitchRollSTD();
 
-        // for(int i = 0; i < angles.size(); i++)
-        //     cout << angles[i]*360/3.14 << " ! ";
+            for(int i = 0; i < angles.size(); i++)
+                cout << angles[i]*360/3.14 << " ! ";
 
-        // cout << endl; 
+            cout << endl; 
+        }
+        catch(...) {
+            cout << "Oops!" << endl;
+        }
 
-        // gpu_frame_1.download(frame_1);
 
 
         // float val = Pi/360; 
@@ -149,14 +155,11 @@ int main(int, char**) {
         // Mat matches_img; 
         // drawMatches(frame_1, keys_1, frame_2, keys_2, matches, matches_img);
 
-        surf.downloadKeypoints(gpu_keys_1, keys_1);
-        surf.downloadKeypoints(gpu_keys_2, keys_2);
-
         gpu_frame_1.download(frame_1);
         gpu_frame_2.download(frame_2);
 
         Mat filtered_matches_img; 
-        drawMatches(frame_1, keys_1, frame_2, keys_2, matches, filtered_matches_img);
+        drawMatches(frame_1, keys_1, frame_2, keys_2, filtered_matches, filtered_matches_img);
         // Mat blurred_img;
         // GaussianBlur(frame_1, blurred_img, cv::Size(0, 0), 3);
         // addWeighted(frame_1, 1.5, blurred_img, -1, 0, blurred_img);
