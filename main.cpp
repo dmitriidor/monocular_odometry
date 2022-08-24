07fe7ffe493d70c937ca68b900c94b737ffbcea8
NaughtiusNaximus <dimador4@gmail.com>
2022-08-24 16:17:03 +0300

    Hoough Lines transform test

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
 #include <opencv4/opencv2/videoio.hpp>
 #include <opencv4/opencv2/features2d/features2d.hpp>
 #include <opencv4/opencv2/imgproc/imgproc.hpp>
-// #include <opencv4/opencv2/core/eigen.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
 #include <opencv4/opencv2/cudafilters.hpp>
 #include <opencv4/opencv2/cudaimgproc.hpp>
@@ -29,7 +30,7 @@ using namespace Eigen;
 int main(int, char**) {
 
 
-    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_3.avi");
+    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_2.avi");
 
     if(!cap.isOpened()){
         cout << "Error opening video stream or file" << endl;
@@ -92,123 +93,33 @@ int main(int, char**) {
         cuda::minMax(gpu_frame_2, &minimum_value, &maximum_value); 
         gpu_frame_2.convertTo(gpu_frame_2, CV_8U, 1/pow(2, 8));
 
-        // surf(gpu_frame_2, cuda::GpuMat(), gpu_keys_2, gpu_descriptors_2);
-        feature_extractor->detectAndCompute(gpu_frame_2, noArray(), keys_2, gpu_descriptors_2);
-
-        vector< vector<DMatch> > matches; 
-        matcher->knnMatch(gpu_descriptors_1, gpu_descriptors_2, matches, 2);
-
-        vector<DMatch> filtered_matches; 
-        double ratio = 0.1; 
-        for(int i = 0; i < matches.size(); i++) {
-            if(matches[i][0].distance < matches[i][1].distance*ratio) {
-                filtered_matches.push_back(matches[i][0]);
-            }
-        }
-
-        // surf.downloadKeypoints(gpu_keys_1, keys_1);
-        // surf.downloadKeypoints(gpu_keys_2, keys_2);
-
-        vector<Point2f> filtered_points_1(filtered_matches.size(), Point2f(0, 0));
-        vector<Point2f> filtered_points_2(filtered_matches.size(), Point2f(0, 0)); 
-        for(int i = 0; i < filtered_matches.size(); i++) {
-            filtered_points_1[i] = keys_1[filtered_matches[i].queryIdx].pt;
-            filtered_points_2[i] = keys_2[filtered_matches[i].trainIdx].pt;
-        }
-
-        double sum_x, sum_y; 
-
-        sum_x = 0; 
-        sum_y = 0;
-        for(int i = 0; i < filtered_points_1.size(); i++){
-            sum_x += filtered_points_1[i].x;
-            sum_y += filtered_points_1[i].y;
-        }
-        Point2f centroid_1 =  Point2f(sum_x/filtered_points_1.size(), sum_y/filtered_points_1.size());
-
-
-        sum_x = 0; 
-        sum_y = 0;
-        for(int i = 0; i < filtered_points_2.size(); i++){
-            sum_x += filtered_points_2[i].x;
-            sum_y += filtered_points_2[i].y;
-        }
-        Point2f centroid_2 =  Point2f(sum_x/filtered_points_2.size(), sum_y/filtered_points_2.size());
-
-        for(int i = 0; i < filtered_points_1.size(); i++){
-            filtered_points_1[i].x -= centroid_1.x;
-            filtered_points_1[i].y -= centroid_1.y;
-        }
-
-
-        for(int i = 0; i < filtered_points_2.size(); i++){
-            filtered_points_2[i].x -= centroid_2.x;
-            filtered_points_2[i].y -= centroid_2.y;
-        }
-
-        Mat points3d_1 = Mat::zeros(Size(filtered_points_1.size(), 3), CV_32F); 
-        Mat points3d_2 = Mat::zeros(Size(filtered_points_1.size(), 3), CV_32F); 
-
-        for(int i = 0; i < points3d_1.cols; i++){
-            points3d_1.at<float>(0, i) = filtered_points_1[i].x;
-            points3d_1.at<float>(1, i) = filtered_points_1[i].y;
-            points3d_1.at<float>(2, i) = 1; 
-        }
-
-        for(int i = 0; i < points3d_2.cols; i++){
-            points3d_2.at<float>(0, i) = filtered_points_2[i].x;
-            points3d_2.at<float>(1, i) = filtered_points_2[i].y;
-            points3d_2.at<float>(2, i) = 1; 
-        }
-
-        Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> pnts3d_1_eigen(points3d_1.ptr<float>(), points3d_1.rows, points3d_1.cols);
-        Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> pnts3d_2_eigen(points3d_2.ptr<float>(), points3d_2.rows, points3d_2.cols);
-
-        if(iter < 4)
-            continue;
-            
-        MatrixXf H = pnts3d_1_eigen * pnts3d_2_eigen.transpose(); 
-
-        cout << H << endl;
-
-        Eigen::JacobiSVD<Eigen::MatrixXf> svd(H, Eigen::ComputeFullV | Eigen::ComputeFullU);
-
-        Matrix3f R = svd.matrixV() * svd.matrixU().transpose(); 
-
-        cout << R << endl;
-
-        if(R.determinant() < 0) { 
-            Eigen::JacobiSVD<Eigen::MatrixXf> svd(R, Eigen::ComputeFullV | Eigen::ComputeFullU);
-            MatrixXf Vt = svd.matrixV(); 
-            Eigen::Vector3f M = Eigen::Vector3f(1, 1, -1);
-            Eigen::Matrix3Xf C = Vt.array().colwise() * M.array();
-            R =  Vt * svd.matrixU().transpose();
-        }
-
-        Vector3f ang = R.eulerAngles(2, 1, 0); 
-        cout << ang*360/3.14 << endl; 
-
-        Vector3f centroid_1_eigen;
-        centroid_1_eigen << centroid_1.x, centroid_1.y, 1; 
-        Vector3f centroid_2_eigen;
-        centroid_2_eigen << centroid_2.x, centroid_2.y, 1; 
-
-        Vector3f T = centroid_2_eigen - centroid_1_eigen;
-
         gpu_frame_1.download(frame_1);
         gpu_frame_2.download(frame_2);
 
-        Mat filtered_matches_img; 
-        drawMatches(frame_1, keys_1, frame_2, keys_2, filtered_matches, filtered_matches_img);
+        Mat dst, cdst, cdstP;
+        Canny(frame_1, frame_1, 50, 200, 3);
+        // threshold(frame_1, frame_1, 100, 255, THRESH_BINARY_INV);
+        // Sobel(frame_1, frame_1, CV_8U, 1, 1, 5);
+        dilate(frame_1, frame_1, Mat(), Point(-1, -1), 1, 1, 1);
+        // erode(frame_1, frame_1, Mat(), Point(-1, -1), 3, 1, 1);
+        // cvtColor(frame_1, cdst, COLOR_GRAY2BGR);
+        cdst = Mat::zeros(frame_1.rows, frame_1.cols, CV_8UC1);
+        cdstP = Mat::zeros(frame_1.rows, frame_1.cols, CV_8UC1);
 
-        Mat shit; 
-        gpu_frame_filtered.download(shit); 
-    
-        imshow("Frame 1", frame_1);
-        imshow("Frame 2", frame_2);
+        vector<Vec4i> linesP; // will hold the results of the detection
+        HoughLinesP(frame_1, linesP, 10, CV_PI/180, 50, 50, 1 ); // runs the actual detection
+        for( size_t i = 0; i < linesP.size(); i++ )
+        {
+            Vec4i l = linesP[i];
+            line( cdstP, Point(l[0], l[1]), Point(l[2], l[3]), 255, 1, LINE_AA);
+        }
+   
+        imshow("Original", frame_1);
+        imshow("Res", cdst);
+        imshow("Inter", cdstP);
         // imshow("TD: ", depthmeter.pipeline.frame_laplacian);
         // imshow("Matches", matches_img);
-        imshow("Filtered Matches", filtered_matches_img);
+        // imshow("Filtered Matches", filtered_matches_img);
 
         waitKey(0);
     }
