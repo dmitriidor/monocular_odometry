396fb26e54514a198aeb948cfc3771746a652b47
NaughtiusNaximus <dimador4@gmail.com>
2022-08-25 10:23:29 +0300

    Template matghing working for linear movements

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ using namespace Eigen;
 int main(int, char**) {
 
 
-    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_2.avi");
+    VideoCapture cap("/home/dmitrii/Downloads/vids/archive/pool_test_3_cut.mp4");
 
     if(!cap.isOpened()){
         cout << "Error opening video stream or file" << endl;
@@ -39,7 +39,7 @@ int main(int, char**) {
 
 
     Mat frame_1, frame_2; 
-    cap.set(CAP_PROP_POS_MSEC, 6000);
+    cap.set(CAP_PROP_POS_MSEC, 0);
     cap >> frame_1;
 
     cuda::GpuMat gpu_frame_1;
@@ -61,21 +61,21 @@ int main(int, char**) {
     cuda::GpuMat gpu_descriptors_1, gpu_descriptors_2; 
 
     gpu_frame_1 = gpu_frame_2; 
-    // surf(gpu_frame_2, cuda::GpuMat(), gpu_keys_2, gpu_descriptors_2);
-    feature_extractor->detectAndCompute(gpu_frame_2, noArray(), keys_2, gpu_descriptors_2);
 
-    // Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_L2);
-    Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_HAMMING);
+    Ptr<cuda::TemplateMatching> matcher = cuda::createTemplateMatching(CV_8U, TM_CCOEFF, Size(0, 0));
+
+    cuda::GpuMat gpu_matching_results; 
+    Mat matching_results;
 
     clock_t start, end; 
     for(int iter = 1; iter < 1000000+1; iter++) {
+        // cap.set(CAP_PROP_POS_MSEC, 1*iter);
         cap >> frame_2;
 
         // gpu_keys_1 = gpu_keys_2; 
         keys_1 = keys_2; 
         gpu_descriptors_1 = gpu_descriptors_2;
         gpu_frame_1 = gpu_frame_2; 
-        
 
         gpu_frame_2.upload(frame_2); 
         gpu_frame_filtered.upload(frame_2);
@@ -93,90 +93,30 @@ int main(int, char**) {
         cuda::minMax(gpu_frame_2, &minimum_value, &maximum_value); 
         gpu_frame_2.convertTo(gpu_frame_2, CV_8U, 1/pow(2, 8));
 
+        Rect region(50, 50, 400, 400);
+        cuda::GpuMat gpu_matching_region = gpu_frame_1(region); 
+        
+        matcher->match(gpu_frame_2, gpu_matching_region, gpu_matching_results); 
+
+        gpu_matching_results.download(matching_results); 
+
+        double minVal, maxVal;
+        Point minLoc, maxLoc;
+        minMaxLoc(matching_results, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+        Mat matching_region;
         gpu_frame_1.download(frame_1);
         gpu_frame_2.download(frame_2);
+        gpu_matching_region.download(matching_region);
 
-        Mat dst, cdst, cdstP;
-        Canny(frame_1, frame_1, 50, 200, 3);
-        // threshold(frame_1, frame_1, 100, 255, THRESH_BINARY_INV);
-        // Sobel(frame_1, frame_1, CV_8U, 1, 1, 5);
-        dilate(frame_1, frame_1, Mat(), Point(-1, -1), 1, 1, 1);
-        // erode(frame_1, frame_1, Mat(), Point(-1, -1), 3, 1, 1);
-        // cvtColor(frame_1, cdst, COLOR_GRAY2BGR);
-        cdst = Mat::zeros(frame_1.rows, frame_1.cols, CV_8UC1);
-        cdstP = Mat::zeros(frame_1.rows, frame_1.cols, CV_8UC1);
+        cout << "dx:" << maxLoc.x - 50 << " dy:" << maxLoc.y - 50 << endl;
 
-        vector<Vec2f> lines; // will hold the results of the detection
-        HoughLines(frame_1, lines, 1, CV_PI/180, 200, 50, 0 ); // runs the actual detection
-        // Draw the lines
-
-        float rho_threshold = 60;
-        float theta_threshold = CV_PI/360*30;
-        vector<vector<int>> similar_lines;
-        // CV_PI/360*20
-        for(int i = 0; i < lines.size(); i++){
-            vector<int> temp {i, 1}; 
-            similar_lines.push_back(temp);
-            for(int j = i; j < lines.size(); j++){
-                if(i == j)
-                    continue; 
-                
-                if(abs(lines[i][0] - lines[j][0]) < rho_threshold && abs(lines[i][1] - lines[j][1]) < theta_threshold){
-                    similar_lines.back()[1]++;
-                }
-            }
-        }
-        
-        vector<bool> flags (similar_lines.size(), true); 
-        sort(similar_lines.begin(), similar_lines.end(), [](auto const& lhs, auto const& rhs) {
-            return lhs[1] > rhs[1];
-            });
-
-        vector<Vec2f> filtered_lines;
-        for(int i = 0; i < similar_lines.size(); i++){
-            if(!flags[i])
-                continue;
-            
-            float rho_avg = 0; 
-            float theta_avg = 0;
-            int similars = 0; 
-            for(int j = i+1; j < similar_lines.size(); j++){
-                if(!flags[j])
-                    continue;
-                
-                if(abs(lines[i][0] - lines[j][0]) < rho_threshold && abs(lines[i][1] - lines[j][1]) < theta_threshold){
-                    flags[j] = false; 
-                    rho_avg += lines[j][0];
-                    theta_avg += lines[j][1];
-                    similars++; 
-                }
-            }
-            filtered_lines.push_back(Vec2f(rho_avg/similars, theta_avg/similars));
-        }
-
-        
-        
-        cout << "Lines: " << lines.size() << endl;
-        cout << "After Filtering: " << filtered_lines.size() << endl;
-
-        for( size_t i = 0; i < filtered_lines.size(); i++ ){
-            float rho = filtered_lines[i][0], theta = filtered_lines[i][1];
-            Point pt1, pt2;
-            double a = cos(theta), b = sin(theta);
-            double x0 = a*rho, y0 = b*rho;
-            pt1.x = cvRound(x0 + 1000*(-b));
-            pt1.y = cvRound(y0 + 1000*(a));
-            pt2.x = cvRound(x0 - 1000*(-b));
-            pt2.y = cvRound(y0 - 1000*(a));
-            line( cdst, pt1, pt2, 255, 3, LINE_AA);
-        }
-   
+        rectangle( frame_2, maxLoc, Point( maxLoc.x + matching_region.cols , maxLoc.y + matching_region.rows ), Scalar::all(0), 2, 8, 0 );
+        rectangle(frame_2, region, Scalar::all(0), 2, 8, 0 );
+        rectangle(frame_1, region, Scalar::all(0), 2, 8, 0 );
         imshow("Original", frame_1);
-        imshow("Res", cdst);
-        // imshow("TD: ", depthmeter.pipeline.frame_laplacian);
-        // imshow("Matches", matches_img);
-        // imshow("Filtered Matches", filtered_matches_img);
-
+        imshow("Res", frame_2);
+        imshow("Region", matching_region);
         waitKey(0);
     }
 };
