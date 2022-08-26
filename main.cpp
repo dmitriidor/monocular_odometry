ce3ba653f8945c72b82765fa49f114dcf1083706
NaughtiusNaximus <dimador4@gmail.com>
2022-08-26 19:19:06 +0300

    Requires fine-tuning

--- main.cpp
+++ main.cpp
@@ -38,20 +38,23 @@ int main(int, char**) {
 
 
     Mat frame_1, frame_2; 
-    cap.set(CAP_PROP_POS_MSEC, 6000);
+    cap.set(CAP_PROP_POS_MSEC, 0);
     cap >> frame_1;
 
     cuda::GpuMat gpu_frame_1;
     cuda::GpuMat gpu_frame_2; 
     cuda::GpuMat gpu_frame_filtered;
 
+    PoseEstimator estimator; 
+    estimator.SetPrevFrame(frame_1); 
+
     gpu_frame_2.upload(frame_1);
     cuda::cvtColor(gpu_frame_2, gpu_frame_2, COLOR_BGR2GRAY);
 
-    Ptr<cuda::Filter> gaussian_filter = cuda::createGaussianFilter(CV_16U, CV_16U, Size(7, 7), 1, 1);
-    Ptr<cuda::Filter> laplace_filter = cuda::createLaplacianFilter(CV_16U, CV_16U, 1); 
+    Ptr<cuda::Filter> gaussian_filter = cuda::createGaussianFilter(CV_8U, CV_8U, Size(7, 7), 1, 1);
+    Ptr<cuda::Filter> laplace_filter = cuda::createLaplacianFilter(CV_8U, CV_8U, 1); 
     // cuda::SURF_CUDA surf(1500, 4, 2, true);
-    Ptr<cuda::ORB> feature_extractor = cuda::ORB::create(30, 1.2f, 8, 31, 0, 2, 0, 20);
+    Ptr<cuda::ORB> feature_extractor = cuda::ORB::create(3000, 1.2f, 8, 30, 0, 2, 0, 31);
 
     vector<KeyPoint> keys_1, keys_2;
     cuda::GpuMat gpu_keys_1, gpu_keys_2;
@@ -61,16 +64,16 @@ int main(int, char**) {
 
     cuda::GpuMat gpu_matching_region_1; 
     cuda::GpuMat gpu_matching_region_2; 
-    Rect region (0, 0, 250, 260);
 
     gpu_frame_1 = gpu_frame_2; 
-    gpu_matching_region_2 = gpu_frame_2(region);
+    gpu_matching_region_2 = gpu_frame_2;
     // surf(gpu_frame_2, cuda::GpuMat(), gpu_keys_2, gpu_descriptors_2);
     feature_extractor->detectAndCompute(gpu_matching_region_2, noArray(), keys_2, gpu_descriptors_2);
 
     // Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_L2);
     Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_HAMMING);
 
+    float tang = 0, tangs = 0; 
 
     clock_t start, end; 
     for(int iter = 1; iter < 1000000+1; iter++) {
@@ -87,19 +90,18 @@ int main(int, char**) {
         gpu_frame_filtered.upload(frame_2);
         cuda::cvtColor(gpu_frame_2, gpu_frame_2, COLOR_BGR2GRAY);
         cuda::cvtColor(gpu_frame_filtered, gpu_frame_filtered, COLOR_BGR2GRAY);
-        gpu_frame_filtered.convertTo(gpu_frame_filtered, CV_16U);
-        cuda::normalize(gpu_frame_filtered, gpu_frame_filtered, 0, pow(2, 16), NORM_MINMAX, CV_16U);
+        gpu_frame_filtered.convertTo(gpu_frame_filtered, CV_8U);
+        cuda::normalize(gpu_frame_filtered, gpu_frame_filtered, 0, pow(2, 8), NORM_MINMAX, CV_8U);
         gaussian_filter->apply(gpu_frame_filtered, gpu_frame_filtered);
         laplace_filter->apply(gpu_frame_filtered, gpu_frame_filtered); 
         double minimum_value, maximum_value; 
         cuda::minMax(gpu_frame_filtered, &minimum_value, &maximum_value);  
-        gpu_frame_2.convertTo(gpu_frame_2, CV_16U);
-        cuda::normalize(gpu_frame_2, gpu_frame_2, 0, pow(2, 16), NORM_MINMAX, CV_16U);
+        gpu_frame_2.convertTo(gpu_frame_2, CV_8U);
+        cuda::normalize(gpu_frame_2, gpu_frame_2, 0, pow(2, 8), NORM_MINMAX, CV_8U);
         cuda::subtract(gpu_frame_2, gpu_frame_filtered, gpu_frame_2);
         cuda::minMax(gpu_frame_2, &minimum_value, &maximum_value); 
-        gpu_frame_2.convertTo(gpu_frame_2, CV_8U, 1/pow(2, 8)); 
 
-        gpu_matching_region_2 = gpu_frame_2(region); 
+        gpu_matching_region_2 = gpu_frame_2; 
 
         // surf(gpu_frame_2, cuda::GpuMat(), gpu_keys_2, gpu_descriptors_2);
         feature_extractor->detectAndCompute(gpu_matching_region_2, noArray(), keys_2, gpu_descriptors_2);
@@ -108,21 +110,36 @@ int main(int, char**) {
         matcher->knnMatch(gpu_descriptors_1, gpu_descriptors_2, matches, 2);
 
         vector<DMatch> filtered_matches; 
-        double ratio = 0.1; 
+        double ratio = 0.8; 
         for(int i = 0; i < matches.size(); i++) {
             if(matches[i][0].distance < matches[i][1].distance*ratio) {
                 filtered_matches.push_back(matches[i][0]);
             }
         }
 
+        vector<DMatch> regionally_filtered_matches; 
+        double max_dist = 20; 
+        for(int i = 0; i < filtered_matches.size(); i++){
+            Point2f p_prev, p_next; 
+            p_prev = keys_1[matches[i][0].queryIdx].pt;
+            p_next = keys_2[matches[i][0].trainIdx].pt;
+
+            float dist_interpoint = sqrt( pow((p_next.x-p_prev.x), 2) + pow((p_next.y-p_prev.y), 2) );
+
+            Point2f p_cntr = Point2f(frame_1.cols/2, frame_1.rows/2); 
+            float dist_center = sqrt( pow((p_next.x-p_cntr.x), 2) + pow((p_next.y-p_cntr.y), 2) );
+            if(dist_interpoint < max_dist * dist_center/p_cntr.x)
+                regionally_filtered_matches.push_back(filtered_matches[i]);
+        }
+
         // surf.downloadKeypoints(gpu_keys_1, keys_1);
         // surf.downloadKeypoints(gpu_keys_2, keys_2);
 
-        vector<Point2f> filtered_points_1(filtered_matches.size(), Point2f(0, 0));
-        vector<Point2f> filtered_points_2(filtered_matches.size(), Point2f(0, 0)); 
-        for(int i = 0; i < filtered_matches.size(); i++) {
-            filtered_points_1[i] = keys_1[filtered_matches[i].queryIdx].pt;
-            filtered_points_2[i] = keys_2[filtered_matches[i].trainIdx].pt;
+        vector<Point2f> filtered_points_1(regionally_filtered_matches.size(), Point2f(0, 0));
+        vector<Point2f> filtered_points_2(regionally_filtered_matches.size(), Point2f(0, 0)); 
+        for(int i = 0; i < regionally_filtered_matches.size(); i++) {
+            filtered_points_1[i] = keys_1[regionally_filtered_matches[i].queryIdx].pt;
+            filtered_points_2[i] = keys_2[regionally_filtered_matches[i].trainIdx].pt;
         }
 
         double sum_x, sum_y; 
@@ -178,8 +195,6 @@ int main(int, char**) {
             
         MatrixXf H = pnts3d_1_eigen * pnts3d_2_eigen.transpose(); 
 
-        cout << H << endl;
-
         Eigen::JacobiSVD<Eigen::MatrixXf> svd(H, Eigen::ComputeFullV | Eigen::ComputeFullU);
 
         Matrix3f R = svd.matrixV() * svd.matrixU().transpose(); 
@@ -192,31 +207,49 @@ int main(int, char**) {
             R =  Vt * svd.matrixU().transpose();
         }
 
+        // cout << R.determinant() << endl; 
+
         Vector3f ang = R.eulerAngles(2, 1, 0); 
-        cout << "Angles: \n" << ang*360/3.14 << endl; 
+        for(int i = 0; i < ang.size(); i++){
+            ang[i] *= 360/CV_PI; 
+            if(ang[i] > 180){
+                ang[i] -= 360;
+            }
+        }
+        tangs += ang[0];
+        for(int i = 0; i < ang.size(); i++)
+            cout << "Angle "<< i << ": " << ang[i] << endl; 
+        cout << "Total: " << tangs << endl; 
 
         Vector3f centroid_1_eigen;
-        centroid_1_eigen << centroid_1.x, centroid_1.y, 1; 
+        centroid_1_eigen << centroid_1.x, centroid_1.y, 1;
         Vector3f centroid_2_eigen;
         centroid_2_eigen << centroid_2.x, centroid_2.y, 1; 
 
         Vector3f T = centroid_2_eigen - centroid_1_eigen;
 
+        // cout << "Translation: \n" << T << endl; 
+
+        estimator.SetNextFrame(frame_2); 
+        if(estimator.CalculateMotion()){
+            tang += estimator.GetYPR()[0];
+            cout << "class angle: " << estimator.GetYPR()[0] << endl;
+            cout << "Class: " << tang << endl;
+        }else{
+            cout << "Doh!" << endl; 
+        }
+
+        imshow("Class", estimator.GetProcessedFrame());
+        // waitKey(0);
+
         gpu_frame_1.download(frame_1);
         gpu_frame_2.download(frame_2);
 
         Mat filtered_matches_img; 
-        drawMatches(frame_1, keys_1, frame_2, keys_2, filtered_matches, filtered_matches_img);
-
-        Mat shit; 
-        gpu_frame_filtered.download(shit); 
-    
-        imshow("Frame 1", frame_1);
-        imshow("Frame 2", frame_2);
-        // imshow("TD: ", depthmeter.pipeline.frame_laplacian);
-        // imshow("Matches", matches_img);
+        drawMatches(frame_1, keys_1, frame_2, keys_2, regionally_filtered_matches, filtered_matches_img);
+        // imshow("Frame 1", frame_1);
+        // imshow("Frame 2", frame_2);
         imshow("Filtered Matches", filtered_matches_img);
-
         waitKey(0);
     }
 };
--- pose_estimator/pose_estimator_class.hpp
+++ pose_estimator/pose_estimator_class.hpp
@@ -1,77 +1,117 @@
 #ifndef POSE_ESTIMATOR_CLASS_H
 #define POSE_ESTIMATOR_CLASS_H
 
+#include <iostream>
+#include <ctime>
+#include <numeric>
+
+#include <eigen3/Eigen/Dense>
+
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/opencv_modules.hpp>
 #include <opencv4/opencv2/videoio.hpp>
 #include <opencv4/opencv2/features2d/features2d.hpp>
 #include <opencv4/opencv2/imgproc/imgproc.hpp>
+// #include <opencv4/opencv2/core/eigen.hpp>
 
 #include <opencv4/opencv2/cudafilters.hpp>
 #include <opencv4/opencv2/cudaimgproc.hpp>
 #include <opencv4/opencv2/cudafeatures2d.hpp>
 #include <opencv4/opencv2/core/cuda.hpp>
-
+#include <opencv4/opencv2/cudaarithm.hpp>
+#include <opencv4/opencv2/xfeatures2d/cuda.hpp>
 
 using namespace cv;
-
+using namespace std; 
+using namespace Eigen; 
 class PoseEstimator{
     public:
-    PoseEstimator(); 
-    // void SetFilterParams(int sample_size, int cycles, float thrshld, int min_inliers);
-    // void SetPreprocessingParams(Size kernel, int laplace_krnl, int bin_thrshld, float thrshld_1, float thrshld_2);
-    // void SetFrame(Mat new_frame); 
+    PoseEstimator() = default; 
+    void SetPrevFrame(Mat frame); 
+    void SetNextFrame(Mat frame); 
+    void SetGaussParams(int stype, int dtype, Size k, float s_x, float s_y);
+    void SetLaplaceParams(int stype, int dtype, int k); 
+    void SetORBParams(int nftrs, float scl, int nlvls, int edgeT, int firstLvl, int WTA_K, int sType, int pSize, int fastT, bool blurD);
+    void SetRatioTestParams(float ratio);
+    void SetRegionTestParams(float distance); 
+    Mat GetProcessedFrame(); 
+    vector<double> GetYPR();
+    vector<double> GetQ();
+    vector<double> GetT();
+    bool CalculateMotion(); 
 
-    // void PreprocessImage();
-    // float CalcVelocities(); 
+    private:
+    void SharpenImage();
+    void ExtractFeatures(); 
+    void ExtractMatches();
+    void FilterMatchesRatio(); 
+    void FilterMatchesRegion();
 
-    // struct FramePipeline { 
-    //     Mat frame_raw; 
-    //     Mat frame_blurred_1;
-    //     Mat frame_gray; 
-    //     Mat frame_laplacian; 
-    //     Mat frame_blurred_2; 
-    //     Mat frame_bin; 
-    //     Mat frame_edges; 
-    // };
-    // FramePipeline pipeline;
+    void Convert3D(); 
+    void CalcNextCentroid(); 
+    void CenterSet(); 
+    void ExtractRotation();
+    void CheckRCase(); 
+    void Convert2YPR(); 
+    void Convert2Q(); 
+    void CalculateTranslation(); 
+    void CycleData(); 
+
+    Mat frame_prev, frame_next; 
+    cuda::GpuMat gpu_frame_prev, gpu_frame_next, gpu_frame_mask;
     
-    // private:
-    // void DetectFAST(); 
-    // void ExtractBEBLID(); 
-    // void FilterRatio();
-    // void CalcRelPose();
-    // void CalcSpeeds();  
+    struct Gaussian{
+        int src_type = CV_8U; 
+        int dst_type = CV_8U;
+        Size ksize = Size(7, 7);
+        float sigma_x = 1;
+        float sigma_y = 1; 
+    };
+    Gaussian pGau; 
 
-    // Mat crnt_frame, prev_frame; 
+    struct Laplace{
+        int src_type = CV_8U; 
+        int dst_type = CV_8U;
+        int ksize = 1; 
+    };
+    Laplace pLap; 
 
-    // Ptr<FastFeatureDetector> detector = FastFeatureDetector::create(10, true);
-    // vector<KeyPoint> crnt_keys, prev_keys;
+    struct ORB{
+        int nfeatures = 3000; 
+        float scale = 1.2;
+        int nlevels = 8;
+        int edgeThreshold = 31;
+        int firstLevel = 0;
+        int WTA_K = 2;
+        int scoreType = 0;
+        int patchSize = 31;
+        int fastThreshold = 20;
+        bool blurForDescriptor = false;
+    }; 
+    ORB pORB;
 
-    // Ptr<Feature2D> extractor = xfeatures2d::BEBLID::create(0.75);
-    // Mat crnt_descriptors, prev_descriptors; 
+    float filter_ratio_ = 0.8; 
+    float filter_distance_ = 20; 
 
-    // BFMatcher matcher;
-    // vector<vector<DMatch>> matches;
+    Ptr<cuda::Filter> gaussian_filter = cuda::createGaussianFilter(pGau.src_type, pGau.dst_type, pGau.ksize, pGau.sigma_x, pGau.sigma_y);
+    Ptr<cuda::Filter> laplacian_filter = cuda::createLaplacianFilter(pLap.src_type, pLap.dst_type, pLap.ksize); 
 
-    // vector<DMatch> matches_1, matches_2;
-    // vector<DMatch> filtered_matches;
+    Ptr<cuda::ORB> feature_extractor = cuda::ORB::create(pORB.nfeatures, pORB.scale, 
+            pORB.nlevels, pORB.edgeThreshold, pORB.firstLevel, pORB.WTA_K, pORB.scoreType, pORB.patchSize);
 
-    // vector<Point2f> crnt_matched, prev_matched;
+    vector<KeyPoint> keys_prev, keys_next;
+    cuda::GpuMat gpu_descriptors_prev, gpu_descriptors_next; 
 
-    // Mat E, R, T;
+    vector< vector<DMatch> > matches;
+    Ptr<cuda::DescriptorMatcher> matcher = cuda::DescriptorMatcher::createBFMatcher(NORM_HAMMING);
 
-    // struct PreprocessingParams {
-    //     Size gaussian_kernel;
-    //     int bin_threshold;
-    //     int laplacian_kernel;
-    //     int canny_threshold_1;
-    //     int canny_threshold_2;
-    // };
-    // PreprocessingParams pp_vals; 
+    vector<DMatch> matches_ratioed, matches_regioned, matches_filtered; 
 
-    // float ratio_filter_thresh_ = 0.8; 
-    // float scale_factor_ = 0; 
+    MatrixXd points3d_prev, points3d_next; 
+    Vector3d centroid_prev, centroid_next;
+    Matrix3d H, R;
+    Vector3d YPR, T; 
+    Quaterniond Q; 
 };
 
 
