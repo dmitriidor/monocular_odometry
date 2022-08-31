0eab038ed923221c9ae6e1909f060f3b1c941bd5
NaughtiusNaximus <dimador4@gmail.com>
2022-08-31 19:10:00 +0300

    Basic working algorithm for rotation and transaltion with matching

--- main.cpp
+++ main.cpp
@@ -20,8 +20,6 @@
 #include <opencv4/opencv2/xfeatures2d/cuda.hpp>
 
 #include "pose_estimator/pose_estimator_class.hpp"
-#include "angle_converter/angle_converter_class.hpp"
-#include "tile_depthmeter/tile_depthmeter_class.hpp"
 
 using namespace std;
 using namespace cv;
@@ -66,15 +64,59 @@ int main(int, char**) {
 
     cuda::GpuMat gpu_matching_results; 
     Mat matching_results;
+    
+    int shift = (frame_1.cols - frame_1.rows)/2; 
+    Rect square_image(shift, 0, frame_1.rows, frame_1.rows);
+
+    int number_of_cells = 16;
+    int cell_size = frame_1.rows/sqrt(number_of_cells);
+
+    int border_size = 10; 
+
+
+    int rows = sqrt(number_of_cells); 
+    int cols = sqrt(number_of_cells);
+    MatrixXi good_cells; 
+    if(number_of_cells%2 == 0){
+        good_cells = MatrixXi(4*2, 2);
+        good_cells <<   0,         cols/2-1, 
+                        0,         cols/2, 
+                        rows/2-1,  0, 
+                        rows/2-1,  cols-1, 
+                        rows/2,    0, 
+                        rows/2,    cols-1,
+                        rows-1,    cols/2-1, 
+                        rows-1,    cols/2;
+    }else{
+        good_cells = MatrixXi(4, 2);
+        good_cells << 0,             (int)cols/2, 
+                      (int)rows/2,   0, 
+                      (int)rows/2,   cols-1, 
+                      rows-1,        (int)cols/2;
+    }
+
+    double tang = 0;
+         
+
+    Rect subregion_for_matching(border_size, border_size, cell_size - 2*border_size, cell_size - 2*border_size);
+    cuda::GpuMat gpu_subregion_1, gpu_subregion_2; 
+    cuda::GpuMat gpu_subregion_to_match, gpu_region_for_matching; 
+
+    int x_new, y_new; 
+    float dx_avg = 0, dy_avg = 0; 
+
+
+    gpu_frame_2 = gpu_frame_1(square_image);
+
+    MatrixXd deltas(good_cells.rows(), 2);
+    MatrixXd positions(good_cells.rows(), 2); 
+    MatrixXd deltas_filtered; 
+    float max_distance = 1; 
 
     clock_t start, end; 
     for(int iter = 1; iter < 1000000+1; iter++) {
-        // cap.set(CAP_PROP_POS_MSEC, 1*iter);
         cap >> frame_2;
 
-        // gpu_keys_1 = gpu_keys_2; 
-        keys_1 = keys_2; 
-        gpu_descriptors_1 = gpu_descriptors_2;
         gpu_frame_1 = gpu_frame_2; 
 
         gpu_frame_2.upload(frame_2); 
@@ -83,7 +125,7 @@ int main(int, char**) {
         cuda::cvtColor(gpu_frame_filtered, gpu_frame_filtered, COLOR_BGR2GRAY);
         gpu_frame_filtered.convertTo(gpu_frame_filtered, CV_16U);
         cuda::normalize(gpu_frame_filtered, gpu_frame_filtered, 0, pow(2, 16), NORM_MINMAX, CV_16U);
-        gaussian_filter->apply(gpu_frame_filtered, gpu_frame_filtered);
+        gaussian_filter->apply(gpu_frame_filtered, gpu_frame_filtered); 
         laplace_filter->apply(gpu_frame_filtered, gpu_frame_filtered); 
         double minimum_value, maximum_value; 
         cuda::minMax(gpu_frame_filtered, &minimum_value, &maximum_value);  
@@ -93,31 +135,166 @@ int main(int, char**) {
         cuda::minMax(gpu_frame_2, &minimum_value, &maximum_value); 
         gpu_frame_2.convertTo(gpu_frame_2, CV_8U, 1/pow(2, 8));
 
-        Rect region(50, 50, 400, 400);
-        cuda::GpuMat gpu_matching_region = gpu_frame_1(region); 
+        gpu_frame_2 = gpu_frame_2(square_image); 
+        dx_avg = 0; 
+        dy_avg = 0;
+
+        int good_cell_counter = 0; 
+        for(int i = 0; i < sqrt(number_of_cells); i++){
+            for(int j = 0; j < sqrt(number_of_cells); j++){
+                if(good_cells.row(good_cell_counter)[0] == i &&  good_cells.row(good_cell_counter)[1] == j){
+                    good_cell_counter++;
+                }else{
+                    continue;
+                }
+                int subregion_shift_x = border_size + cell_size * i;
+                int subregion_shift_y = border_size + cell_size * j;
+                Rect subregion_to_match(subregion_shift_x, subregion_shift_y, cell_size - 2*border_size, cell_size - 2*border_size); 
+                gpu_subregion_to_match = gpu_frame_1(subregion_to_match);
+
+                int region_shift_x = cell_size * i;
+                int region_shift_y = cell_size * j;
+                Rect region_for_matching(region_shift_x, region_shift_y, cell_size, cell_size);
+                gpu_region_for_matching = gpu_frame_2(region_for_matching);
+                matcher->match(gpu_region_for_matching, gpu_subregion_to_match, gpu_matching_results);
+                
+                gpu_matching_results.download(matching_results); 
+                double minVal, maxVal;
+                Point minLoc, maxLoc;
+                minMaxLoc(matching_results, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+                x_new = maxLoc.x - border_size;
+                y_new = maxLoc.y - border_size; 
+                deltas.row(good_cell_counter-1) << x_new, y_new; 
+                positions.row(good_cell_counter-1) << subregion_shift_x, subregion_shift_y; 
+
+
+                // cout << "dx:" << x_new - border_size << " dy:" << y_new - border_size << endl;
+
+                dx_avg += x_new; 
+                dy_avg += y_new; 
+
+
+                gpu_frame_1.download(frame_1);
+                gpu_frame_2.download(frame_2);
+                rectangle(frame_2, maxLoc + Point(region_shift_x, region_shift_y), 
+                                Point(maxLoc.x + gpu_subregion_to_match.cols +region_shift_x, maxLoc.y + gpu_subregion_to_match.rows+region_shift_y), Scalar::all(0), 2, 8, 0 );
+                rectangle(frame_2, region_for_matching, Scalar::all(0), 2, 8, 0);
+                rectangle(frame_2, subregion_to_match, Scalar::all(0), 2, 8, 0);
+                rectangle(frame_1, region_for_matching, Scalar::all(0), 2, 8, 0);
+                rectangle(frame_1, subregion_to_match, Scalar::all(0), 2, 8, 0);
+                imshow("Matching result", frame_2);
+                imshow("1st frame", frame_1);
+                waitKey(0); 
+
+                if(good_cell_counter == good_cells.rows()){
+                    goto loop_exit; 
+                }
+            }
+        }
         
-        matcher->match(gpu_frame_2, gpu_matching_region, gpu_matching_results); 
+        loop_exit:
 
-        gpu_matching_results.download(matching_results); 
+        int best_score = 0; 
+        float max_distance = 5; 
+        MatrixXd deltas_filtered(0, 2); 
+        MatrixXd position_filtered(0, 2); 
+        for(int i = 0; i < deltas.rows(); i++){
+            for(int j = i+1; j < deltas.rows(); j++){
+                Vector2d centroid; 
+                centroid << 0, 0; 
+                centroid = (deltas.row(i) + deltas.row(j))/2;
 
-        double minVal, maxVal;
-        Point minLoc, maxLoc;
-        minMaxLoc(matching_results, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+                int score = 0;
+                MatrixXd deltas_inliers(0, 2);
+                MatrixXd positions_inliers(0, 2);
+                for(int k = 0; k < deltas.rows(); k++){
+                    if((deltas.row(k)-centroid.transpose()).norm() < max_distance){
+                        score++; 
+                        deltas_inliers.conservativeResize(deltas_inliers.rows()+1, NoChange);
+                        deltas_inliers.row(deltas_inliers.rows()-1) = deltas.row(k); 
 
-        Mat matching_region;
-        gpu_frame_1.download(frame_1);
-        gpu_frame_2.download(frame_2);
-        gpu_matching_region.download(matching_region);
+                        positions_inliers.conservativeResize(positions_inliers.rows()+1, NoChange);
+                        positions_inliers.row(positions_inliers.rows()-1) = positions.row(k); 
+                    }
 
-        cout << "dx:" << maxLoc.x - 50 << " dy:" << maxLoc.y - 50 << endl;
+                    if(score > best_score){
+                        deltas_filtered = deltas_inliers; 
+                        position_filtered = positions_inliers; 
+                    }
+                }
+            }
+        }
 
-        rectangle( frame_2, maxLoc, Point( maxLoc.x + matching_region.cols , maxLoc.y + matching_region.rows ), Scalar::all(0), 2, 8, 0 );
-        rectangle(frame_2, region, Scalar::all(0), 2, 8, 0 );
-        rectangle(frame_1, region, Scalar::all(0), 2, 8, 0 );
-        imshow("Original", frame_1);
-        imshow("Res", frame_2);
-        imshow("Region", matching_region);
-        waitKey(0);
+        MatrixXd pnts3d_1_eigen(deltas_filtered.rows(), 3); 
+        MatrixXd pnts3d_2_eigen(deltas_filtered.rows(), 3); 
+        for(int i = 0; i < deltas_filtered.rows(); i++){
+            pnts3d_2_eigen.row(i) <<    deltas_filtered.row(i)[0] + position_filtered.row(i)[0],
+                                        deltas_filtered.row(i)[1] + position_filtered.row(i)[1],
+                                        1; 
+            pnts3d_1_eigen.row(i) <<    position_filtered.row(i)[0],
+                                        position_filtered.row(i)[1],
+                                        1; 
+        }
+
+        Vector3d centroid_1, centroid_2;
+
+        centroid_1 <<   pnts3d_1_eigen.col(0).mean(), 
+                        pnts3d_1_eigen.col(1).mean(),
+                        pnts3d_1_eigen.col(2).mean(); 
+        
+        centroid_2 <<   pnts3d_2_eigen.col(0).mean(), 
+                        pnts3d_2_eigen.col(1).mean(),
+                        pnts3d_2_eigen.col(2).mean(); 
+
+        for(int i = 0; i < pnts3d_2_eigen.rows(); i++){
+            pnts3d_2_eigen.row(i) -= centroid_2; 
+            pnts3d_1_eigen.row(i) -= centroid_1;
+        }
+        
+
+        if(pnts3d_2_eigen.rows() < 3)
+            continue;
+
+        MatrixXd H = pnts3d_1_eigen.transpose() * pnts3d_2_eigen; 
+
+        cout << "\nH: \n" << H << endl;
+
+        Eigen::JacobiSVD<Eigen::MatrixXd> svd(H, Eigen::ComputeFullV | Eigen::ComputeFullU);
+
+        Matrix3d R = svd.matrixV() * svd.matrixU().transpose(); 
+
+        if(R.determinant() < 0) { 
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd(R, Eigen::ComputeFullV | Eigen::ComputeFullU);
+            MatrixXd Vt = svd.matrixV(); 
+            Eigen::Vector3d M = Eigen::Vector3d(1, 1, -1);
+            Eigen::Matrix3d C = Vt.array().colwise() * M.array();
+            R =  Vt * svd.matrixU().transpose();
+        }
+
+        Vector3d ang = R.eulerAngles(2, 1, 0); 
+        for(int i = 0; i < ang.size(); i++){
+            ang[i] *= 360/CV_PI; 
+            if(ang[i] > 180){
+                ang[i] -= 360;
+            }
+        }
+        tang += ang[0];
+        for(int i = 0; i < ang.size(); i++)
+            cout << "Angle "<< i << ": " << ang[i] << endl; 
+        cout << "Total angle: " << tang << endl; 
+
+        Vector3d T = centroid_2 - centroid_1;
+        cout << "Translation: " << T << endl; 
+
+        // Vector3f centroid_1_eigen;
+        // centroid_1_eigen << centroid_1.x, centroid_1.y, 1;
+        // Vector3f centroid_2_eigen;
+        // centroid_2_eigen << centroid_2.x, centroid_2.y, 1; 
+
+        // Vector3f T = centroid_2_eigen - centroid_1_eigen;
+
+        // cout << "Averages: " << deltas_filtered.col(0).mean() << " " << deltas_filtered.col(1).mean() << endl; 
     }
 };
 
--- pose_estimator/pose_estimator_class.hpp
+++ pose_estimator/pose_estimator_class.hpp
@@ -1,83 +1,95 @@
-#ifndef POSE_ESTIMATOR_CLASS_H
-#define POSE_ESTIMATOR_CLASS_H
+// #ifndef POSE_ESTIMATOR_CLASS_H
+// #define POSE_ESTIMATOR_CLASS_H
 
-#include <iostream>
-#include <ctime>
-#include <numeric>
+// #include <iostream>
+// #include <ctime>
+// #include <numeric>
 
-#include <eigen3/Eigen/Dense>
+// #include <eigen3/Eigen/Dense>
 
-#include <opencv4/opencv2/opencv.hpp>
-#include <opencv4/opencv2/opencv_modules.hpp>
-#include <opencv4/opencv2/videoio.hpp>
-#include <opencv4/opencv2/features2d/features2d.hpp>
-#include <opencv4/opencv2/imgproc/imgproc.hpp>
-// #include <opencv4/opencv2/core/eigen.hpp>
+// #include <opencv4/opencv2/opencv.hpp>
+// #include <opencv4/opencv2/opencv_modules.hpp>
+// #include <opencv4/opencv2/videoio.hpp>
+// #include <opencv4/opencv2/features2d/features2d.hpp>
+// #include <opencv4/opencv2/imgproc/imgproc.hpp>
+// // #include <opencv4/opencv2/core/eigen.hpp>
 
-#include <opencv4/opencv2/cudafilters.hpp>
-#include <opencv4/opencv2/cudaimgproc.hpp>
-#include <opencv4/opencv2/cudafeatures2d.hpp>
-#include <opencv4/opencv2/core/cuda.hpp>
-#include <opencv4/opencv2/cudaarithm.hpp>
-#include <opencv4/opencv2/xfeatures2d/cuda.hpp>
+// #include <opencv4/opencv2/cudafilters.hpp>
+// #include <opencv4/opencv2/cudaimgproc.hpp>
+// #include <opencv4/opencv2/cudafeatures2d.hpp>
+// #include <opencv4/opencv2/core/cuda.hpp>
+// #include <opencv4/opencv2/cudaarithm.hpp>
+// #include <opencv4/opencv2/xfeatures2d/cuda.hpp>
 
-using namespace cv;
-using namespace std; 
-using namespace Eigen; 
-class PoseEstimator{
-    public:
-    PoseEstimator() = default; 
-    void SetPrevFrame(Mat frame); 
-    void SetNextFrame(Mat frame); 
-    void SetGaussParams(int stype, int dtype, Size k, float s_x, float s_y);
-    void SetLaplaceParams(int stype, int dtype, int k); 
-    Mat GetProcessedFrame(); 
-    vector<double> GetYPR();
-    vector<double> GetQ();
-    vector<double> GetT();
-    bool CalculateMotion(); 
+// using namespace cv;
+// using namespace std; 
+// using namespace Eigen; 
+// class PoseEstimator{
+//     public:
+//     PoseEstimator() = default; 
+//     void SetPrevFrame(Mat frame); 
+//     void SetNextFrame(Mat frame); 
+//     void SetGaussParams(int stype, int dtype, Size k, float s_x, float s_y);
+//     void SetLaplaceParams(int stype, int dtype, int k); 
+//     void SetMatcherParams(int number_of_cells, int border_size, int max_spread); 
+//     Mat GetProcessedFrame(); 
+//     vector<double> GetYPR();
+//     vector<double> GetQ();
+//     vector<double> GetT();
+//     bool CalculateMotion(); 
 
-    private:
-    void SharpenImage();
+//     private:
+//     void SharpenImage();
+//     void SetMatrix();
+//     void ExtractMotion(); 
+//     void FilterMoves(); 
+//     void Convert3D(); 
+//     void CalcNextCentroid(); 
+//     void CenterSet(); 
+//     void ExtractRotation();
+//     void CheckRCase(); 
+//     void Convert2YPR(); 
+//     void Convert2Q(); 
+//     void CalculateTranslation(); 
+//     void CycleData(); 
 
-    void Convert3D(); 
-    void CalcNextCentroid(); 
-    void CenterSet(); 
-    void ExtractRotation();
-    void CheckRCase(); 
-    void Convert2YPR(); 
-    void Convert2Q(); 
-    void CalculateTranslation(); 
-    void CycleData(); 
-
-    Mat frame_prev, frame_next; 
-    cuda::GpuMat gpu_frame_prev, gpu_frame_next, gpu_frame_mask;
+//     Mat frame_prev, frame_next; 
+//     cuda::GpuMat gpu_frame_prev, gpu_frame_next, gpu_frame_mask;
     
-    struct Gaussian{
-        int src_type = CV_8U; 
-        int dst_type = CV_8U;
-        Size ksize = Size(7, 7);
-        float sigma_x = 1;
-        float sigma_y = 1; 
-    };
-    Gaussian pGau; 
+//     struct Gaussian{
+//         int src_type = CV_8U; 
+//         int dst_type = CV_8U;
+//         Size ksize = Size(7, 7);
+//         float sigma_x = 1;
+//         float sigma_y = 1; 
+//     };
+//     Gaussian pGau; 
 
-    struct Laplace{
-        int src_type = CV_8U; 
-        int dst_type = CV_8U;
-        int ksize = 1; 
-    };
-    Laplace pLap; 
+//     struct Laplace{
+//         int src_type = CV_8U; 
+//         int dst_type = CV_8U;
+//         int ksize = 1; 
+//     };
+//     Laplace pLap; 
 
-    Ptr<cuda::Filter> gaussian_filter = cuda::createGaussianFilter(pGau.src_type, pGau.dst_type, pGau.ksize, pGau.sigma_x, pGau.sigma_y);
-    Ptr<cuda::Filter> laplacian_filter = cuda::createLaplacianFilter(pLap.src_type, pLap.dst_type, pLap.ksize); 
+//     struct TMatcher{ 
+//         int number_of_cells = 16; 
+//         int border_size = 10; 
+//         int max_spread = 1; 
+//     };
+//     TMatcher pMat; 
 
-    MatrixXd points3d_prev, points3d_next; 
-    Vector3d centroid_prev, centroid_next;
-    Matrix3d H, R;
-    Vector3d YPR, T; 
-    Quaterniond Q; 
-};
+//     Ptr<cuda::Filter> gaussian_filter = cuda::createGaussianFilter(pGau.src_type, pGau.dst_type, pGau.ksize, pGau.sigma_x, pGau.sigma_y);
+//     Ptr<cuda::Filter> laplacian_filter = cuda::createLaplacianFilter(pLap.src_type, pLap.dst_type, pLap.ksize); 
+
+//     MatrixXd deltas, positions; 
+//     MatrixXd deltas_filtered, positions_filtered; 
+//     MatrixXd points3d_prev, points3d_next; 
+//     Vector3d centroid_prev, centroid_next;
+//     Matrix3d H, R;
+//     Vector3d YPR, T; 
+//     Quaterniond Q; 
+// };
 
 
-#endif
+// #endif
